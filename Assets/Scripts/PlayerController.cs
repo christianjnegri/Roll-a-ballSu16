72bd7494b16b8a320d325589c5857fbfdb1a8f02
christianjnegri <christianjnegri@gmail.com>
2016-06-02 08:04:08 -0400

    Roll-a-Ball GUI
    GUI Added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,15 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	private int count;
+	public GUIText countText;
+	public GUIText winText;
+
+	void Start(){
+		count = 0;
+		SetCountText();
+		winText.text = "";
+	}
 
 	void FixedUpdate(){
 		float moveHorizontal = Input.GetAxis ("Horizontal");
@@ -16,6 +25,15 @@ public class PlayerController : MonoBehaviour {
 	void OnTriggerEnter(Collider other){
 		if (other.gameObject.tag == "Pickup") {
 			other.gameObject.SetActive(false);
+			count++;
+			SetCountText();
 		}
 	}
+	void SetCountText(){
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 16) {
+			winText.text = "YOU WIN!";
+		}
+	}
+	
 }
